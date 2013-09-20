e368e46ade15d8a04e309b18e6ffd6fb4c7b0133
Eugen <netoctone@gmail.com>
2013-09-20 18:21:50 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ang::Application.config.secret_token = '40ec41dda41175c6069547fe479bf75eedc52b75d7a2212b8101fc7108b11c7e2a0fb4c8ffb043353186f4c65ff0af20c27f0de89696b86644b68f4060806948'
